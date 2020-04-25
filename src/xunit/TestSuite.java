ee443046c9146887d0269d092c0c703c01a2f3e7
choyeachan <choyuichan@gmail.com>
2020-04-25 22:53:44 +0900

    test 로 시작하는 테스트 메서드 자동으로 testSuite에 추가
    https://youtu.be/tdKFZcZSJmg (1:42:29)

--- src/xunit/TestSuite.java
+++ src/xunit/TestSuite.java
@@ -2,10 +2,29 @@ package src.xunit;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class TestSuite implements Test {
 
 	List<Test> tests = new ArrayList<>();
+
+	public TestSuite(Class<? extends Test> testClass) {
+
+		Arrays.stream(testClass.getMethods())
+			.filter(m -> m.getName().startsWith("test"))
+			.forEach(m -> {
+				try {
+					add(testClass.getConstructor(String.class).newInstance(m.getName()));
+				}catch(Exception e){
+					new RuntimeException(e);
+				}
+					
+			});
+
+	}
+
+	public TestSuite(){
+	}
 
 	public void add(Test test){
 		tests.add(test);
--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -6,12 +6,5 @@ public class XUnitTest {
 		TestResult result = new TestResult();
 		suite.run(result);
 		System.out.println(result.getSummary());
-
-
-		TestSuite suite2 = TestCaseTest.suite();
-		suite2.add(new TestCaseTest("testRunning"));
-		TestResult result2 = new TestResult();
-		suite2.run(result2);
-		System.out.println(result2.getSummary());
 	}
 }
